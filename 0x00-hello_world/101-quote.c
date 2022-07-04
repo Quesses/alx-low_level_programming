3d6418f5c873cd5abe03d37e0a44379de7c0824b
Quesses <quesses1@gmail.com>
2022-07-04 02:38:05 -0700

    C is fun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the standard error sandwiched around the standard outpu/t
+ * main - Entry point
  *
  * Return: 1 , print to standard error
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar - 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar - 2015-10-19\n", 59);
 	return (1);
 }
