04e1e06a857d197ef3d1d8111eca315f547619d9
Quesses <quesses1@gmail.com>
2022-07-04 16:37:00 -0700

    c is cool

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the single digits of base 10 using the putchar function
+ *
+ * Return: Always 0 on success.
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
+}
