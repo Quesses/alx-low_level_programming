b317dff2d828fe931bab128b25ad01a603459965
Quesses <quesses1@gmail.com>
2022-07-04 16:04:31 -0700

    Betty is annoying

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets in lowercase excluding the letters q and e
+ *
+ * Return: Always 0 on success.
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <+ 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}	
+	putchar('\n');
+	return (0);
+
+}
