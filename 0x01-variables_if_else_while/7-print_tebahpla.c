341d8b4bcfbda5ab1b383f2b702451f4d3768694
Quesses <quesses1@gmail.com>
2022-07-04 16:51:29 -0700

    C is real fun

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabets in reverse
+ *
+ * Return: Always 0 on success.
+ */
+int main(void)
+{
+	char alphabet;
+	
+	for (alphabet = 122; alphabet >= 97; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+
+}
