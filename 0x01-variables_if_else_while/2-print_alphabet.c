2af7124f8ea627fcc7d345f4b3c705207b927239
Quesses <quesses1@gmail.com>
2022-07-04 15:42:22 -0700

    C is really fun

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets in lowercase
+ *
+ * Return: Always 0 on success.
+ */
+int main(void)
+{
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets in lowercase followed by uppercase
+ *
+ * Return: Always 0 on success.
+ */
+int main(void)
+{
+	int ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
