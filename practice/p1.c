bc5aa880375f20a0caaf13eb324fb4edc6fddd2d
Quesses <quesses1@gmail.com>
2022-07-05 05:14:51 -0700

    wow

---
+++ practice/p1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: this is me practicing on ma own
+ */
+int main(void)
+{
+	int answer = 1;
+	int i;
+	for(i=2; 1<=10; i++)
+	{
+		answer = answer * i;
+	}
+	printf("Answer = %d\n", answer);
+}
