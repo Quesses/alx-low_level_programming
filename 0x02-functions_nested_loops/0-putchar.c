dd1885009f2151dbf1ebf7e782e7356540b00c4d
Quesses <quesses1@gmail.com>
2022-07-05 22:00:13 -0700

    wow

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	char str[] = "_putchar";
+	char str() = "_putchar";
 	int i = 0;
 
-	while (str[i] = '\0')
+	while (str(i) = '\0')
 	{
-		char c = str[i];
+		char c = str(i);
 		_putchar (c);
 		i++;
 	}
