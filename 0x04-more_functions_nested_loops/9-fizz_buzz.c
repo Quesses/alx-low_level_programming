bb94422f9105e5e214625bdf77ef7473b843bb3f
Quesses <quesses1@gmail.com>
2022-07-09 02:03:51 +0000

    wow

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,29 +9,19 @@
  * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
-	int i;
-
 	for (int i = 1; 1 <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz\n");
+		else if (i % 3 == 0)
+			printf("Fizz\n");
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
-		else if (i % 3 ==0)
-		{
-			printf("Fizz");
-		}
 		else
-		{
-			printf("%d", i);
-		}
-		printf("\n");
+			printf("%d\n", i);	
 	}
+
 	return (0);
 }
