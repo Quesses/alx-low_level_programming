b0ec92bbc1b966f781328fa61de09c940ea10e4d
Quesses <quesses1@gmail.com>
2022-07-15 00:45:58 +0000

    C!

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * *@s1: first string to be compared
+ * *@s2: second ring to be compared
+ *
+ * Return: +/-/0 depending on the comparison.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+	return (0);
+}
