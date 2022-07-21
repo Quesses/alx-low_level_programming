05830c541753968340bc3d06f1d069caa6050abd
Quesses <quesses1@gmail.com>
2022-07-21 01:21:21 +0000

    C recursion, WOW!

---
+++ 0x08-recursion/100-is_palinbdrome.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_palindrome - determines if a given string is a palindrome or not
+ * @s: given string to be checked
+ *
+ * Return: 1 if it is a palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	int
