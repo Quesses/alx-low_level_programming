d8599892afecb9722700eec19079458e263cf07c
Quesses <quesses1@gmail.com>
2022-07-26 21:02:04 +0000

    C!

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,17 +18,14 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-
-       	str = (char *) malloc(size * sizeof(char));
-	if (str = NULL)
+	str = (char *) malloc(size * sizeof(char));
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
 	}
-
 	return (str);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,24 +12,19 @@
 char *_strdup(char *str)
 {
 	char *dup;
-	int len = 0;
-	int i;
-	
+	int i, len = 0;
+
 	if (str == NULL)
 		return (NULL);
-	
 	while (str[len] != '\0')
 		len++;
-
-	dup = (char *) malloc((sizeof(char) *len) +1);
+	dup = (char *) malloc((sizeof(char) * len) + 1);
 	if (dup == NULL)
 		return (NULL);
-	
 	for (i = 0; i < len; i++)
 	{
 		dup[i] = str[i];
 	}
 	dup[len] = '\0';
-		
 	return (dup);
 }
