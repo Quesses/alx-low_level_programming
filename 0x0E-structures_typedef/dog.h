3f343b8d38d2153464eecb1d2f652a9ef5a879e7
Quesses <quesses1@gmail.com>
2022-08-02 01:41:46 +0000

    C!

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: floating number age
  * @owner: pointer to string owner
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
