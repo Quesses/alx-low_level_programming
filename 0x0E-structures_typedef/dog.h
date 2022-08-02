2dc6cf7e02a2daeab76c53b61b84bf3697f4554c
Quesses <quesses1@gmail.com>
2022-08-02 01:02:34 +0000

    C!

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct type containing age, name and owner
+ * @name: pointer to string name
+ * @age: floating number age
+ * @owner: pointer to string owner
+ */
 struct dog
 {
 char *name;
