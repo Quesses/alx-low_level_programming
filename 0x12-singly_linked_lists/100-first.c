096bdf1c048496ae7f53545842e3eb4c1c5e2736
Quesses <quesses1@gmail.com>
2022-08-13 00:45:01 +0000

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first_exe - function that runs before the main function
+ */
+
+void first_exe(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
