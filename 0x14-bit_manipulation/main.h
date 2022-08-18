97af0ac05051715154c909a1fd94a91786203dca
Quesses <quesses1@gmail.com>
2022-08-18 20:32:24 +0000

    task 1 done

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,7 @@
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+void print_b_helper(unsigned long int n, unsigned long int i);
+int _putchar(char c);
 
 #endif
