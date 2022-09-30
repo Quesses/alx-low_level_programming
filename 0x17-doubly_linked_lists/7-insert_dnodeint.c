319a05cf893dd48087da343c59e1ebe94ef5cfc4
Quesses <quesses1@gmail.com>
2022-09-30 01:41:55 +0000

    all done C! wow. finally

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		newnode->next = temp->next;
 		newnode->prev = temp;
-		temp->next->prev= newnode;
+		temp->next->prev = newnode;
 		temp->next = newnode;
 	}
 	else
