a3006b5cb9614af0e903299a4c2c3376771614b2
Quesses <quesses1@gmail.com>
2022-10-23 14:42:26 +0000

    task 0, why??

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,7 +8,7 @@ void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *tmp, *node;
 	unsigned long int i;
-	hash_table_t hash = ht;
+	hash_table_t *hash = ht;
 
 	for (i = 0; i < ht->size; i++)
 	{
